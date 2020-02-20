b42ae17fc8f82fdcfadbf05a310be35d4b024306
hyphen <hyphen.call@gmail.com>
2020-02-20 11:55:49 +0530

    adding new item to a budgetController

--- BudgetApp/app.js
+++ BudgetApp/app.js
@@ -14,15 +14,52 @@ var budgetController = (function () {
         this.description = description,
         this.value = value
     }
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    }
+
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+
+            // Create new ID
+            if(data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            // Create new item based on 'inc' or 'exp' type
+            if(type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if(type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+            
+            // Push it into our data structure
+            data.allItems[type].push(newItem)
+
+            // Return the new element
+            return newItem;
+        },
+
+        testing: function() {
+            console.log(data);
+        }
+    };
+
     
 })();
 
-
-var Income = function(id, description, value) {
-    this.id = id, 
-    this.description = description,
-    this.value = value
-}
 
 /**
  * @UI CONTROLLER
@@ -68,12 +105,14 @@ var controller = (function(budgCtrl, UICtrl) {
         });
     }
     var ctrlAddItem = function() {
+        var input, newItem;
+
         // 1. Get the field input data
-        var input = UICtrl.getInput();
-        console.log(input);
+        input = UICtrl.getInput();
 
         // 2. Add the item to the budget controller
- 
+        newItem = budgCtrl.addItem(input.type, input.description, input.value);
+
         // 3. Add the item to the UI
         
         // 4. Calculate the budget
