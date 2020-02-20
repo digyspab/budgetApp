5331c30ab54414782d44f4bf92a274252ccb67b3
hyphen <hyphen.call@gmail.com>
2020-02-20 20:30:29 +0530

    update totalBudget percentage and total income and expenses and console

--- BudgetApp/app.js
+++ BudgetApp/app.js
@@ -14,6 +14,14 @@ var budgetController = (function () {
         this.value = value;
     };
 
+    var calculateTotal = function(type) {
+        var sum = 0;
+        data.allItems[type].forEach((cur) => {
+            sum += cur.value;
+        });
+        data.totals[type] = sum;
+    };
+
     var data = {
         allItems: {
             exp: [],
@@ -23,7 +31,10 @@ var budgetController = (function () {
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+
+        budget: 0,
+        percentage: -1
     };
 
     return {
@@ -49,6 +60,32 @@ var budgetController = (function () {
 
             // Return the new element
             return newItem;
+        },
+
+        calculateBudget:  function() {
+
+            // Calculate total income and expenses
+            calculateTotal('exp');
+            calculateTotal('inc');
+            
+            // Calculate the budget: income - expenses
+            data.budget = data.totals.inc - data.totals.exp;
+
+            // Calculate the percentage of income that we spent
+            if(data.totals.inc > 0) {
+                data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100)
+            } else {
+                data.percentage = -1;
+            }
+        },
+
+        getBudget: function() {
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            }
         },
 
         testing: function () {
@@ -125,12 +162,6 @@ var UIController = (function () {
         clearFilelds: function() {
             var fields, fieldsArr;
             fields = document.querySelectorAll(`${DOMstrings.inputValue}, ${DOMstrings.inputDescription}`);
-
-        },
-
-        clearFilelds: function() {
-            var fields, fieldsArr;
-            fields = document.querySelectorAll(`${DOMstrings.inputValue}, ${DOMstrings.inputDescription}`);
             fieldsArr = Array.prototype.slice.call(fields);
 
             fieldsArr.forEach((current, index, array) => {
@@ -157,6 +188,7 @@ var controller = (function (budgCtrl, UICtrl) {
         document
             .querySelector(DOM.inputBtn)
             .addEventListener("click", ctrlAddItem);
+
         document.addEventListener("keypress", function (evnent) {
             if (evnent.keyCode === 13 || event.which === 13) {
                 ctrlAddItem();
@@ -167,10 +199,13 @@ var controller = (function (budgCtrl, UICtrl) {
     var updateBudget = function() {
 
         // 1. Calculate the budget
+        budgCtrl.calculateBudget();
 
         // 2. Return the budget
+        var budget = budgCtrl.getBudget();
 
         // 6. Display the budget on the UI
+        console.log(budget);
     }
     
     var ctrlAddItem = function () {
